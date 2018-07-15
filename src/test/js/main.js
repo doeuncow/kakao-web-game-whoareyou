db6db2a53aef68cd4d11bc33383d4c944aea4f81
20163093 <kimsung97@naver.com>
2018-07-15 15:48:03 +0900

    재업로드

--- src/test/js/main.js
+++ src/test/js/main.js
@@ -5,37 +5,19 @@ function preload(){
 
   game.load.image("correct", "./images/correct.png");
   game.load.image("wrong", "./images/wrong.png");
-
-  // game.load.spritesheet("correct", "correct.png", 50, 50);
-  // game.load.spritesheet("wrong", "wrong.png", 50, 50);
 }
 function create(){
   game.physics.startSystem(Phaser.Physics.ARCADE);
   game.stage.backgroundColor = "#eee8aa"; //배경색 지정
-
-  // game.stage.backgroundColor = "#F5F5DC"; //배경색 지정
-  // //인터폰 테두리(색은 아직..)
-  // game.create.texture('up', ['A'], 1000, 80, 0);
-  // game.add.sprite(0,0,'up');
-  // game.create.texture('left', ['A'], 80, 600, 0);
-  // game.add.sprite(0,0,'left');
-  // game.create.texture('right', ['A'], 80, 600, 0);
-  // game.add.sprite(880,0,'right');
-  // game.create.texture('down', ['A'], 1000, 80, 0);
-  // game.add.sprite(0,560,'down');
-  door = game.add.group();
-  door.create(180,27,"door");
-
-  correct = game.add.group();
-  correct.create(20,100,"correct");
-
-  // correct = game.add.sprite(200, 200, "correct");
-  // game.physics.arcade.enable(correct);
-  // wrong_button = game.add.sprite(0, 300, "wrong");
-  // game.physics.arcade.enable(wrong_button);
+  var image = game.add.image(0, 0, "door"); //배경이미지 0, 0 좌표에 지정
 
   game.scale.pageAlignHorizontally = true; //화면 가운데위치
   game.scale.pageAlignVertically = true; //화면 가운데위치
+
+  correct = game.add.sprite(55,400,"correct");
+  game.physics.arcade.enable(correct);
+  wrong = game.add.sprite(800,400,"wrong");
+  game.physics.arcade.enable(wrong);
 }
 function update(){
 
